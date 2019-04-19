9b8a10bc7bcdcef30eeb3c730eabcb4725024247
Cory Logan <he@corylogan.com>
2019-04-19 15:35:13 -0400

    change it up

--- routes.js
+++ routes.js
@@ -1,12 +1,34 @@
 const emailCheck = require('email-check')
+
+const emailCheck = require('email-check')
+
+
+const checker = (email, count) => {
+  return emailCheck(email, {
+    from: 'he@corylogan.com',
+    host: 'mx.zoho.com',
+    timeout: 3000
+  }).then(function (res) {
+    if(count < 1 && res === false) {
+      console.log('likely bad, retrying once');
+      return checker(email, count + 1)
+    } else {
+      return res
+    }
+  }).catch(function (err) {
+    console.log('this is surely bad, returned an error')
+    return false
+  });
+}
+
+// const email = 'he@corylogan.com'
+// checker(email, 0).then((result) => {
+//         console.log(email + ' ' + result)
+// })
 var appRouter = function (app) {
   app.post("/", function(req, res) {
-    console.log(req.body.email)
-    emailCheck(req.body.email, {
-      from: 'he@corylogan.com',
-      host: 'mx.zoho.com',
-      timeout: 4000
-    }).then(function (res) {
+    emailCheck(req.body.email, 0)
+    .then(function (res) {
       res.status(200).send({valid: res});
     }).catch(function (err) {
       res.status(200).send({valid: false});
@@ -15,14 +37,3 @@ var appRouter = function (app) {
 }
 
 module.exports = appRouter;
-
-
-// emailCheck('he@corylogan.com', {
-//   from: 'he@corylogan.com',
-//   host: 'mx.zoho.com',
-//   timeout: 5000
-// }).then(function (res) {
-//   console.log(res);
-// }).catch(function (err) {
-//   console.error('looks invalid');
-// });
